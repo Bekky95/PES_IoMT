98737c7d1cd121947b9f4242286a69f0184570d1
Lucian Perujo <2120915@stud.hs-mannheim.de>
2026-05-10 21:31:55 +0200

    added uart task and changed some more data handling stuff. TODO the queues dont seem to be working as intended? either the adc is firing too fast or the UI isnt updating or calling tick()

--- IoTM_/TouchGFX/gui/src/model/Model.cpp
+++ IoTM_/TouchGFX/gui/src/model/Model.cpp
@@ -2,7 +2,7 @@
 #include <gui/model/ModelListener.hpp>
 #include "../../STM32CubeIDE/App/uiQueue.h"
 #include "SensorHandler/SensorHandler.h"
-
+extern osMessageQueueId_t uiQueue;
 Model::Model() : modelListener(0)
 {
 
@@ -13,11 +13,14 @@ void Model::tick()
     SensorData data;
     //Get Semaphore and read data until queue is empty
     // TODO: check timing issues, adc could be writing here too fast and this could be blocking
-
-	while(xQueuePeek(SensorHandler::instance().getUIQueue(), &data, 0) == pdTRUE) {
+    uint32_t cnt = osMessageQueueGetCount(uiQueue);
+	while(cnt > 0) {
 		// Read and remove item from queue
-		xQueueReceive(SensorHandler::instance().getUIQueue(), &data, 0);
-		modelListener->onSensorUpdated(data);
+		if(osMessageQueueGet(uiQueue, &data, 0, 0) == osOK ) {
+			//	xQueueReceive(SensorHandler::instance().getUIQueue(), &data, 0);
+			modelListener->onSensorUpdated(data);
+
+		}
 	}
 
 }
